95541c3a059b39264e70df5daa2d24f03b45f4d5
TDDDaniel <danielturcu21@gmail.com>
2026-07-06 10:26:39 +0300

    feat: afisare erori de validare per camp la register + stil .error

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,10 +10,19 @@
     <h1>Inregistrare</h1>
 
     <form method="POST" action="/register">
+        <!--
+            Am adaugat logica erorilor la fiecare input 
+        -->
         @csrf
-        <input type="text" name="name" placeholder="Nume">
-        <input type="email" name="email" placeholder="Email">
+        <input type="text" name="name" placeholder="Nume" value="{{ old('name') }}">
+        @error('name') <span class="error">{{ $message }}</span> @enderror
+
+        <input type="email" name="email" placeholder="Email" value="{{ old('email') }}">
+        @error('email') <span class="error">{{ $message }}</span> @enderror
+
         <input type="password" name="password" placeholder="Parola">
+        @error('password') <span class="error">{{ $message }}</span> @enderror
+
         <button type="submit">Inregistreaza-te</button>
     </form>
     <p class="link">ai cont? <a href="/login">Autentificare</a></p>
